274804ce8bbffb8e362869905e5c1e73cda25c5b
Vuyisa Baza <vuyisa.baza@albatrosgolf.co.za>
2024-06-29 15:14:40 +0200

    created and finished the main nav bar

---
+++ src/components/common/menu-dropdown/MenuDropdown.tsx
@@ -0,0 +1,35 @@
+import { useState } from 'react';
+import './MenuDropdown.less';
+
+interface MenuDropdownProps {
+  title: string;
+  items: Array<string>;
+}
+
+const MenuDropdown = ({ title, items }: MenuDropdownProps) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div className="dropdown">
+      <a className="dropdown-toggle" onClick={toggleDropdown}>
+        {title}
+        <span className="dropdown-arrow"></span>
+      </a>
+      {isOpen && (
+        <ul className="dropdown-menu">
+          {items.map((item, index) => (
+            <li key={index} className="dropdown-item">
+              {item}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default MenuDropdown;
