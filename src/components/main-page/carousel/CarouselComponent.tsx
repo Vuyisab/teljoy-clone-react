dfcc1d1323847077fafc3df3e2827638aadc899e
Vuyisa Baza <vuyisa.baza@albatrosgolf.co.za>
2024-07-10 10:17:03 +0200

    added image overlay

--- src/components/main-page/carousel/CarouselComponent.tsx
+++ src/components/main-page/carousel/CarouselComponent.tsx
@@ -9,6 +9,7 @@ const CarouselComponent = () => {
       {products.map(product => (
         <Carousel.Item>
           <img className="w-100 carousel-image" src={product.imageurl} alt={product.name} />
+          <div className="carousel-overlay"></div>
           <Carousel.Caption>
             <h3>{product.name}</h3>
             <p>{product.description}</p>
